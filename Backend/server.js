02ad46d77ea0f8ab7e95520534fbb3b6a623c14a
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-04-08 14:39:18 +0530

    setted the dev environment

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const express=require("express")
+const http=require("http")
+const cors=require("cors")
+const socketio=require("socket.io")
+const app=express()
+const server=http.createServer(app)
+const io=socketio(server,{cors:{origin:"*"}})
+const jwt=require("jsonwebtoken")
