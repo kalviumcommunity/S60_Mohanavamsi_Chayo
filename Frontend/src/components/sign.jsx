5e2f89ba72497358a1694b5f23201ee4a5f9957b
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-04-12 12:56:20 +0530

    added

--- Frontend/src/components/sign.jsx
+++ Frontend/src/components/sign.jsx
@@ -72,7 +72,7 @@ function Sigin() {
    bg-gray-700 text-white px-6 py-2 rounded-lg
    border-white
    border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
-   active:border-b-[2px] active:brightness-90 active:translate-y-[2px] hover:shadow-xl hover:shadow-green-300 shadow-green-300 active:shadow-none"
+   active:border-b-[2px] active:brightness-90 active:translate-y-[2px] hover:shadow-xl hover:shadow-white  active:shadow-none"
    onClick={submit}>
      {load ? "Loading" : "Sign"}
    </button>
